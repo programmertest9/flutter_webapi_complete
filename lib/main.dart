91eab1404d893ee07473dd3c35ec86c6619e4ec2
programmertest9 <programmer.test.th@gmail>
2022-07-25 14:35:01 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_webapi/routes.dart';
 import 'package:flutter_webapi/themes/style.dart';
 
-void main()  => runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
